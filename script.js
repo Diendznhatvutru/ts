ce749d8490a428e6f0edfbecf39c136d9c10de82
Diendznhatvutru <95350608+Diendznhatvutru@users.noreply.github.com>
2024-10-11 22:04:43 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validateLogin() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-    window.location.href = 'dashboard.html'; 
-    return false;
+  // Lưu thông tin xác thực bất kỳ (không kiểm tra tính hợp lệ)
+  localStorage.setItem("isAuthenticated", "true"); // Lưu thông tin xác thực
+  alert("Đăng nhập thành công!"); // Hiển thị thông báo đăng nhập thành công
+  window.location.href = "dashboard.html"; // Chuyển hướng đến trang dashboard
+  return false; // Ngăn không cho form được submit
 }
